349eb8764a5bded6415643535ca2c47de337b113
Rich Fecher <rfecher@gmail.com>
2020-11-18 13:20:07 -0500

    updates from pahse 1.5

--- RUL_Model.py
+++ RUL_Model.py
@@ -66,90 +66,88 @@ def rec_plot(s, eps=0.10, steps=10):
     return Z
 
 def pad_series(test_df, sequence_length=sequence_length):
-	groupby = test_df.groupby('id')['cycle'].max()
-	nrows = groupby[groupby<=50]
-	over_50 = test_df[~test_df.id.isin(nrows.index)]
+    groupby = test_df.groupby('id')['cycle'].max()
+    nrows = groupby[groupby<=50]
+    over_50 = test_df[~test_df.id.isin(nrows.index)]
 
-	for unit in nrows.index:
-	    temp = test_df[test_df.id == unit]
-	    padding = pd.DataFrame()
-	    nmissing = 51 - len(temp)
-	    #Create synthetic starter rows
-	    for i in range(nmissing):
-	        padding = padding.append(pd.DataFrame(temp.iloc[0]).transpose(), ignore_index=True)
-	    #Combine synthetic starter padding with available data
-	    temp = padding.append(temp, ignore_index=True)
-	    #Renumber cycles
-	    temp.cycle = range(1,len(temp)+1)
-	    #Append new padded series to over 50, 
-	    over_50 = over_50.append(temp, ignore_index=True)
-	#Reorder dataframe by id and cycle
-	over_50 = over_50.sort_values(by=['id', 'cycle'])
-	return over_50
+    for unit in nrows.index:
+        temp = test_df[test_df.id == unit]
+        padding = pd.DataFrame()
+        nmissing = 51 - len(temp)
+        #Create synthetic starter rows
+        for i in range(nmissing):
+            padding = padding.append(pd.DataFrame(temp.iloc[0]).transpose(), ignore_index=True)
+        #Combine synthetic starter padding with available data
+        temp = padding.append(temp, ignore_index=True)
+        #Renumber cycles
+        temp.cycle = range(1,len(temp)+1)
+        #Append new padded series to over 50, 
+        over_50 = over_50.append(temp, ignore_index=True)
+    #Reorder dataframe by id and cycle
+    over_50 = over_50.sort_values(by=['id', 'cycle'])
+    return over_50
 
 def preprocess(test_df):
-	### Rename and Parse raw columns
-	if len(test_df.columns == 28):
-		test_df.drop(test_df.columns[[26, 27]], axis=1, inplace=True)
-	test_df.columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3',
-	                     's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
-	                     's15', 's16', 's17', 's18', 's19', 's20', 's21']
-	### SCALE TEST DATA ###
-	for col in test_df.columns:
-	    if col[0] == 's':
-	        test_df[col] = scale(test_df[col])
-	test_df = test_df.dropna(axis=1)
+    ### Rename and Parse raw columns
+    if len(test_df.columns) > 2:
+        test_df.drop(test_df.columns[[26, 27]], axis=1, inplace=True)
+    test_df.columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3',
+                         's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
+                         's15', 's16', 's17', 's18', 's19', 's20', 's21']
+    ### SCALE TEST DATA ###
+    for col in test_df.columns:
+        if col[0] == 's':
+            test_df[col] = scale(test_df[col])
+    test_df = test_df.dropna(axis=1)
 
-	### Pad Sequences with under 51 cycles
-	test_df = pad_series(test_df)
+    ### Pad Sequences with under 51 cycles
+    test_df = pad_series(test_df)
 
-	### GENERATE X TEST ###
-	x_test = []
-	#Currently Hard Coded based on features showing variance in train file 1
-	sequence_cols = ['setting1', 'setting2', 's2', 's3', 's4', 's6', 's7', 's8', 's9', 's11', 's12', 's13', 's14', 's15', 's17', 's20', 's21']
-	for engine_id in test_df.id.unique():
+    ### GENERATE X TEST ###
+    x_test = []
+    #Currently Hard Coded based on features showing variance in train file 1
+    sequence_cols = ['setting1', 'setting2', 's2', 's3', 's4', 's6', 's7', 's8', 's9', 's11', 's12', 's13', 's14', 's15', 's17', 's20', 's21']
+    for engine_id in test_df.id.unique():
 
-	    for sequence in gen_sequence(test_df[test_df.id==engine_id], sequence_length, sequence_cols):
-	        x_test.append(sequence)
-	    
-	x_test = np.asarray(x_test)
+        for sequence in gen_sequence(test_df[test_df.id==engine_id], sequence_length, sequence_cols):
+            x_test.append(sequence)
+        
+    x_test = np.asarray(x_test)
 
-	### TRANSFORM X TRAIN TEST IN IMAGES ###
+    ### TRANSFORM X TRAIN TEST IN IMAGES ###
 
-	x_test_img = np.apply_along_axis(rec_plot, 1, x_test).astype('float16')
+    x_test_img = np.apply_along_axis(rec_plot, 1, x_test).astype('float16')
 
-	return test_df, x_test_img
+    return test_df, x_test_img
 
 # Generate Class Predictions
 def predict(x_test_img):
-	"""Returns the class labels, one for each sliding window."""
-	return model.predict_classes(x_test_img)
+    "Returns the class labels, one for each sliding window."
+    return model.predict_classes(x_test_img)
 
 def preprocess_and_predict(test_df, output='RUL', sequence_length=sequence_length):
-	"""Returns one prediction per unit. Output can be 'RUL' or 'Category'."""
-	test_df, x_test_img = preprocess(test_df)
-	class_predictions = predict(x_test_img)
-	test_df = test_df[test_df['cycle']>sequence_length]
-	test_df['class_prediction'] = class_predictions
-	test_df['RUL_prediction'] = test_df['class_prediction'].map(rul_dict)
-	return test_df
+    "Returns one prediction per unit. Output can be 'RUL' or 'Category'."
+    test_df, x_test_img = preprocess(test_df)
+    class_predictions = predict(x_test_img)
+    test_df = test_df[test_df['cycle']>sequence_length]
+    test_df['class_prediction'] = class_predictions
+    test_df['RUL_prediction'] = test_df['class_prediction'].map(rul_dict)
+    return test_df
 
 def summarize_predictions_by_unit(test_df_output, col='RUL_prediction'):
-	"""Input should already have class_prediction and RUL_prediction columns.
-	You can chain this with preprocess_and_predict. For example:
-	rul_predictions = summarize_predictions_by_unit(preprocess_and_predict(test_df))
-	"""
-	return test_df_output.groupby('id').tail(1)[col].values
+    "Input should already have class_prediction and RUL_prediction columns. You can chain this with preprocess_and_predict. For example: rul_predictions = summarize_predictions_by_unit(preprocess_and_predict(test_df))"
+    return test_df_output.groupby('id').tail(1)[col].values
 
 def test(test_df):
-	rul_predictions = summarize_predictions_by_unit(preprocess_and_predict(test_df))
-	return rul_predictions
+    rul_predictions = summarize_predictions_by_unit(preprocess_and_predict(test_df))
+    return rul_predictions
 
-
+def my_test():
+    print('testing')
 #Retraining
 
-def preprocess_train(train_df, w = [200,175,150,125,100,75,50,40,30,25,20,15,10,5]):
-    if len(train_df.columns == 28):
+def preprocess_train(train_df, w = [5]):
+    if len(train_df.columns) == 28:
         train_df.drop(train_df.columns[[26, 27]], axis=1, inplace=True)
     train_df.columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3',
                          's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
@@ -157,12 +155,15 @@ def preprocess_train(train_df, w = [200,175,150,125,100,75,50,40,30,25,20,15,10,
     print('#id:',len(train_df.id.unique()))
     train_df = train_df.sort_values(['id','cycle'])
 
+    max_cycle = train_df['cycle'].max()
     ### CALCULATE RUL TRAIN ###
     train_df['RUL']=train_df.groupby(['id'])['cycle'].transform(max)-train_df['cycle']
 
     train_df['label'] = np.where(train_df['RUL'] <= w[0], 1, 0 )
     for i in range(1,len(w)):
         train_df.loc[train_df['RUL'] <= w[i], 'label'] = i+1
+        
+    train_df = train_df[train_df['cycle'] < max_cycle - w[-1]]
 
     ### SCALE Train DATA ###
     for col in train_df.columns:
@@ -170,7 +171,10 @@ def preprocess_train(train_df, w = [200,175,150,125,100,75,50,40,30,25,20,15,10,
             train_df[col] = scale(train_df[col])
 
     train_df = train_df.dropna(axis=1)
-
+    ### Pad Sequences with under 51 cycles
+    train_df = pad_series(train_df)
+    
+    
     ### SEQUENCE COL: COLUMNS TO CONSIDER ###
     # sequence_cols = []
     # for col in train_df.columns:
@@ -202,11 +206,11 @@ def preprocess_train(train_df, w = [200,175,150,125,100,75,50,40,30,25,20,15,10,
     y_train = np.asarray(y_train).reshape(-1,1)
 
     ### ENCODE LABEL ###
-    y_train = to_categorical(y_train)
+    y_train = to_categorical(y_train,2)
 
     return x_train_img, y_train
 
-def train_model(x_train_img, y_train, w = [200,175,150,125,100,75,50,40,30,25,20,15,10,5], sequence_length = 50, smoothing_coef=0.01):
+def train_model(x_train_img, y_train, w = [5], sequence_length = 50, smoothing_coef=0.01):
     n_cats = len(w) + 1
 
     model = Sequential()
